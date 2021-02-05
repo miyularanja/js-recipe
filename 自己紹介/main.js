5f67a8cea12be90b0ed615bddf8a74bfd11752f6
Takehiko Yazawa <Yazawafamily@Takehiko-no-MacBook-Pro.local>
2021-02-05 17:46:38 +0900

    new

--- 自己紹介/main.js
+++ 自己紹介/main.js
@@ -4,23 +4,46 @@ let hobby = document.getElementById("hobby")
 const qbutton1 = document.getElementById("q-button1")
 const qbutton2 = document.getElementById("q-button2")
 const qbutton3 = document.getElementById("q-button3")
+const q2button1 = document.getElementById("q2button1")
+const q2button2 = document.getElementById("q2button2")
+const q2button3 = document.getElementById("q2button3")
 
 school.onclick = function(){
  school.textContent = "埼玉工業大学"
 }
+school.ondblclick = function(){
+  school.textContent = "情報システム"
+}
 birthday.onclick = function(){
   birthday.textContent = "2001/05/08"
+}
+birthday.ondblclick = function(){
+  birthday.textContent = "19"
 }
 hobby.onclick = function(){
   hobby.textContent ="ダンス"
 }
+hobby.ondblclick = function(){
+  hobby.textContent ="ゴルフ"
+}
 
 qbutton1.onclick = function(){
-  qbutton1.textContent = "残念"
+  qbutton1.textContent = "残念！"
 }
 qbutton2.onclick = function(){
   qbutton2.textContent = "正解！"
 }
 qbutton3.onclick = function(){
-  qbutton3.textContent = "残念"
+  qbutton3.textContent = "残念！"
+}
+// 選択肢のCSSが使えない。
+
+q2button1.onclick = function(){
+  q2button1.textContent = "残念！"
+}
+q2button2.onclick = function(){
+  q2button2.textContent = "正解！"
+}
+q2button3.onclick = function(){
+  q2button3.textContent ="惜しい！今度焼肉屋になります"
 }
